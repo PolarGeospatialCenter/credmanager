e0e4182f7e6510d6b2f4f4534508a25a6d617ceb
Andrew Zenk <azenk@umn.edu>
2018-01-03 13:46:00 -0600

    Create credmanager client

---
+++ client/client.go
@@ -0,0 +1,153 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+
+	vault "github.com/hashicorp/vault/api"
+	credmanagertypes "github.umn.edu/pgc-devops/credmanager-api/types"
+)
+
+type ErrClientError struct {
+	Err error
+}
+
+func (e ErrClientError) Error() string {
+	return fmt.Sprintf("this error shouldn't occur, it's likely a bug in the client: %v", e.Err)
+}
+
+var (
+	// ErrInvalidTokenFormat returned when the token format is invalid
+	ErrInvalidTokenFormat = errors.New("Token format is invalid")
+	// ErrServerError is returned when the server returns a 500 or something else that's not our fault
+	ErrServerError = errors.New("credmanager api server returned an error that wasn't our fault")
+	// ErrTokenRequestDenied is returned when the server denies our request for a token
+	ErrTokenRequestDenied = errors.New("request for a token was denied")
+	// ErrBadResponse
+	ErrBadResponse = errors.New("unable to parse server response")
+	// Internal Errors
+	errNoTokenFile = errors.New("token file doesn't exist")
+)
+
+// HTTPClient Interface specifies the pieces of http.Client that we use and provides
+// an interface for testing purposes
+type HTTPClient interface {
+	Do(r *http.Request) (*http.Response, error)
+}
+
+type TokenUnwrapper interface {
+	Unwrap(string) (*vault.Secret, error)
+}
+
+// ValidTokenFormat validates the format of a vault token returns true if valid
+func ValidTokenFormat(token string) bool {
+	tokenRegexp, _ := regexp.Compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
+	return tokenRegexp.MatchString(token)
+}
+
+// CredManagerClient retrieves a vault token from a credmanager api server and
+// stores it locally so that it can be used as long as it remains valid.
+type CredManagerClient struct {
+	ServerUrl string
+	TokenFile string
+	Hostname  string
+}
+
+func (cm *CredManagerClient) GetToken(httpClient HTTPClient, unwrapper TokenUnwrapper) (string, error) {
+	// Do we have an unwrapped token stored already?
+	if tokenString, err := cm.loadToken(); err == nil {
+		return tokenString, nil
+	}
+
+	wrappingTokenString, err := cm.requestToken(httpClient)
+	if err != nil {
+		return "", err
+	}
+
+	secret, err := unwrapper.Unwrap(wrappingTokenString)
+	if err != nil {
+		log.Fatalf("Unable to unwrap token %s\n", err)
+	}
+
+	tokenString := secret.Auth.ClientToken
+	if !ValidTokenFormat(tokenString) {
+		return "", fmt.Errorf("invalid token returned from server")
+	}
+
+	return tokenString, cm.saveToken(tokenString)
+}
+
+func (cm *CredManagerClient) requestToken(httpClient HTTPClient) (string, error) {
+	tokenRequest := &credmanagertypes.TokenRequest{Hostname: cm.Hostname}
+	body, err := json.Marshal(tokenRequest)
+	if err != nil {
+		return "", ErrClientError{fmt.Errorf("error marshaling request: %v", err)}
+	}
+	r, err := http.NewRequest("POST", cm.ServerUrl, bytes.NewBuffer(body))
+	if err != nil {
+		return "", ErrClientError{fmt.Errorf("error creating credmanager request: %v", err)}
+	}
+	response, err := httpClient.Do(r)
+	if err != nil {
+		return "", ErrClientError{fmt.Errorf("error making http request to credmanager server: %v", err)}
+	}
+	switch response.StatusCode {
+	case http.StatusOK:
+		tokenResponse := &credmanagertypes.TokenResponse{}
+		responseBody, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return "", ErrClientError{fmt.Errorf("error reading response body into string: %v", err)}
+		}
+		err = json.Unmarshal(responseBody, tokenResponse)
+		if err != nil {
+			return "", ErrBadResponse
+		}
+
+		if !ValidTokenFormat(tokenResponse.Token) {
+			return "", ErrBadResponse
+		}
+		return tokenResponse.Token, nil
+	case http.StatusForbidden:
+		return "", ErrTokenRequestDenied
+	case http.StatusBadRequest:
+		return "", ErrClientError{fmt.Errorf("bad request")}
+	case http.StatusInternalServerError:
+		return "", ErrServerError
+	default:
+		return "", fmt.Errorf("server returned an unexpected error: %s", response.Status)
+	}
+}
+
+func (cm *CredManagerClient) loadToken() (string, error) {
+	filename := cm.TokenFile
+
+	_, err := os.Stat(filename)
+	if err != nil && os.IsNotExist(err) {
+		return "", errNoTokenFile
+	} else if err != nil {
+		return "", err
+	}
+
+	token, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	tokenString := string(token)
+	if !ValidTokenFormat(tokenString) {
+		return "", ErrInvalidTokenFormat
+	}
+
+	return tokenString, nil
+}
+
+func (cm *CredManagerClient) saveToken(token string) error {
+	return ioutil.WriteFile(cm.TokenFile, []byte(token), 0600)
+}
---
+++ client/client_test.go
@@ -0,0 +1,175 @@
+package client
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	vault "github.com/hashicorp/vault/api"
+	credmanagertypes "github.umn.edu/pgc-devops/credmanager-api/types"
+)
+
+func TestValidToken(t *testing.T) {
+	valid := "30171EE0-62F9-423F-B287-EAEFCFD9A2FF"
+	invalid := []string{
+		"vli43va47gvbwb347LFU3V7R4LBGR",
+		"as30171EE0-62F9-423F-B287-EAEFCFD9A2FFd",
+		"fd30171EE0-62F9-423F-B287-EAEFCFD9A2FF",
+		"30171EE0-62F9-423FB287-EAEFCFD9A2FF",
+		"30171EE0-62F9-423Z-B287-EAEFCFD9A2FF",
+	}
+	if !ValidTokenFormat(valid) {
+		t.Errorf("Valid token failed validation")
+	}
+
+	for _, invalidToken := range invalid {
+		if ValidTokenFormat(invalidToken) {
+			t.Errorf("Invalid token didn't fail as expected: %s", invalidToken)
+		}
+	}
+}
+
+type fakeHTTPClient struct {
+	Status        int
+	TokenResponse *credmanagertypes.TokenResponse
+}
+
+func (f *fakeHTTPClient) Do(r *http.Request) (*http.Response, error) {
+	w := httptest.NewRecorder()
+	switch f.Status {
+	case http.StatusOK:
+		w.WriteHeader(f.Status)
+		w.Header().Set("Content-type", "application/json")
+		body, err := json.Marshal(f.TokenResponse)
+		if err != nil {
+			return nil, err
+		}
+		w.Write(body)
+	case 0:
+		w.WriteHeader(http.StatusInternalServerError)
+	default:
+		w.WriteHeader(f.Status)
+	}
+	return w.Result(), nil
+}
+
+func TestRequestToken(t *testing.T) {
+	cm := &CredManagerClient{Hostname: "sample-0-0"}
+
+	token, err := cm.requestToken(&fakeHTTPClient{Status: http.StatusOK, TokenResponse: &credmanagertypes.TokenResponse{Token: "221ECFD7-E093-45EF-8070-E1FA284A06C0"}})
+	if err != nil {
+		t.Errorf("Request failed: %v", err)
+	}
+
+	if token != "221ECFD7-E093-45EF-8070-E1FA284A06C0" {
+		t.Errorf("Wrong token returned: %s", token)
+	}
+
+	_, err = cm.requestToken(&fakeHTTPClient{Status: http.StatusOK, TokenResponse: &credmanagertypes.TokenResponse{Token: "221ECFD7sdfs"}})
+	if err != ErrBadResponse {
+		t.Errorf("Bad token accepted as valid or other error: %v", err)
+	}
+
+	_, err = cm.requestToken(&fakeHTTPClient{Status: http.StatusInternalServerError})
+	if err != ErrServerError {
+		t.Errorf("Request didn't return a server error as expected, actual error: %v", err)
+	}
+
+	_, err = cm.requestToken(&fakeHTTPClient{Status: http.StatusBadGateway})
+	if err.Error() != "server returned an unexpected error: 502 Bad Gateway" {
+		t.Errorf("Wrong error returned for unhandled responses: %v", err)
+	}
+
+	_, err = cm.requestToken(&fakeHTTPClient{Status: http.StatusForbidden})
+	if err != ErrTokenRequestDenied {
+		t.Errorf("Request didn't return token request denied error as expected, actual err: %v", err)
+	}
+}
+
+func TestLoadNonExistTokenFile(t *testing.T) {
+	cm := &CredManagerClient{TokenFile: "/tmp/badfile-221ECFD7-E093-45EF-8070-E1FA284A06C0"}
+	_, err := cm.loadToken()
+	if err != errNoTokenFile {
+		t.Fatalf("Temporary token file exists, or it's non-existence isn't being detected properly: %v", err)
+	}
+}
+
+func TestSaveAndLoadToken(t *testing.T) {
+	tempFile, err := ioutil.TempFile("", "testtoken")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tempFile.Name())
+
+	sampleToken := "221ECFD7-E093-45EF-8070-E1FA284A06C0"
+
+	cm := &CredManagerClient{TokenFile: tempFile.Name()}
+
+	_, err = cm.loadToken()
+	if err != ErrInvalidTokenFormat {
+		t.Fatalf("Empty temporary token file isn't recognized as an invalid token format error: %v", err)
+	}
+
+	err = cm.saveToken(sampleToken)
+	if err != nil {
+		t.Fatalf("Failed to save sample token: %v", err)
+	}
+
+	loadedToken, err := cm.loadToken()
+	if err != nil {
+		t.Errorf("Unable to load saved token: %v", err)
+	}
+	if loadedToken != sampleToken {
+		t.Errorf("Token loaded from file doesn't match the one saved: loaded '%s', saved '%s'", loadedToken, sampleToken)
+	}
+}
+
+type testUnwrapper struct {
+	WrappedToken string
+}
+
+func (f *testUnwrapper) Unwrap(string) (*vault.Secret, error) {
+	return &vault.Secret{Auth: &vault.SecretAuth{ClientToken: f.WrappedToken}}, nil
+}
+
+func TestGetToken(t *testing.T) {
+	tempFile, err := ioutil.TempFile("", "testtoken")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Remove tempfile because we just need the name
+	os.Remove(tempFile.Name())
+	// Defer a second remove since we're expecting it to be created by the client
+	defer os.Remove(tempFile.Name())
+
+	cm := &CredManagerClient{TokenFile: tempFile.Name(), Hostname: "sample-0-0"}
+
+	// our fake api server should return a valid token to simulate a wrapping token
+	fakeAPIServer := &fakeHTTPClient{Status: http.StatusOK, TokenResponse: &credmanagertypes.TokenResponse{Token: "221ECFD7-E093-45EF-8070-E1FA284A06C0"}}
+
+	realToken := "A89B3399-1A16-42EB-BE83-4CCDF9166884"
+	token, err := cm.GetToken(fakeAPIServer, &testUnwrapper{WrappedToken: realToken})
+	if err != nil {
+		t.Fatalf("An error ocurred while getting the token: %v", err)
+	}
+
+	if token != realToken {
+		t.Errorf("GetToken returned an incorrect token: expected '%s', got '%s'", realToken, token)
+	}
+
+	// tempfile should exist now, reloading
+	badToken := "BADB3399-1A16-42EB-BE83-4CCDF9166884"
+	token, err = cm.GetToken(fakeAPIServer, &testUnwrapper{WrappedToken: badToken})
+	if err != nil {
+		t.Fatalf("An error ocurred while getting the token from file: %v", err)
+	}
+
+	if token != realToken {
+		t.Errorf("GetToken returned an incorrect token while re-reading from file: expected '%s', got '%s'", realToken, token)
+	}
+
+}
